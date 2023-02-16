cb443116c28f9b9e43f82200d1ea95aa63f36a49
panchambhagat <tejas.bhagat94@gmail.com>
2023-02-16 01:30:19 -0800

    varibleAssign.js
    Personal Details

---
+++ 01_varibleAssign.js
@@ -0,0 +1,24 @@
+console.log("*********Step1**********");
+
+var fullName = "Tejas Bhagat"; 
+console. log(fullName);
+var fullName = "Tejas Panchamrao Bhagat"
+console.log(fullName);
+console.log("*********Step2***********");
+var myLove = "I Love Only JS"
+console.log(myLove);
+console.log("**********Step3***********");
+var firstName = "Tejas"
+console.log("firstName:",firstName);
+var lastName = "Bhagat"
+console.log("lastName:",lastName);
+var age = "28"
+console.log("age:",age);
+var address = "Hinganghat"
+console.log("address:",address);
+var pinCode = "442301"
+console.log("pincode:",pinCode);
+var gender = "male"
+console.log("gender:",gender);
+var isMarried = "NO"
+console.log("isMarried:",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,5 @@
 console.log("Tejas Bhagat");
+var fullName = "Tejas Bhagat"
+console.log(fullName);
+
+var city;   
