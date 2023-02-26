ac365d1d029f956d7990f89beb6bb299bb784311
panchambhagat <tejas.bhagat94@gmail.com>
2023-02-26 02:02:25 -0800

    01_swapVariable.js
    Swapping name and number

---
+++ 01_swapVariable.js
@@ -0,0 +1,16 @@
+console.log("swapping Variable 01");
+var sweety = "sweety"; //cutie
+var cutie = "cutie";  //sweety
+var temp = "sweety";
+var sweety = "cutie";
+var cutie = "sweety";
+console.log(sweety,cutie);
+console.log("swapping variable 02");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+var num1 = num2;
+var num2 = num3;
+var num3 = num1;
+console.log(num1,num2,num3);
+
